472d308927348cf742504b39d4e45046dfb672ac
Ana Luísa Fogarin <ana.luisa.fogarin@gmail.com>
2023-06-14 21:45:48 -0300

    fix: change root directory in pathmapper

--- src/util/path_mapper.py
+++ src/util/path_mapper.py
@@ -3,12 +3,12 @@ import os
 
 class PathMapper():
     def __init__(self):
-        if "tcc" in os.getcwd() and "src" in os.getcwd():
+        if "SoFH-Solar-Flares-Hunter" in os.getcwd() and "src" in os.getcwd():
             self.icon_path = "gui" + os.sep + "icons"
-        elif "tcc" in os.getcwd() and "src" not in os.getcwd():
+        elif "SoFH-Solar-Flares-Hunter" in os.getcwd() and "src" not in os.getcwd():
             self.icon_path = "src" + os.sep + "gui" + os.sep + "icons"
         else:
-            self.icon_path = "tcc" + os.sep +  "src" + os.sep + "gui" + os.sep + "icons"
+            self.icon_path = "SoFH-Solar-Flares-Hunter" + os.sep +  "src" + os.sep + "gui" + os.sep + "icons"
 
     def generate_icon_path(self, string):
         return os.path.join(self.icon_path, string)
